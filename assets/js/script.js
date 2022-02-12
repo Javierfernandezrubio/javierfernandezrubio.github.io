3e65b7c1d9211f4c73fa3d60aeceb34e9a46da5b
Javier Fernandez Rubio <a19feruja@iesgrancapitan.org>
2022-02-12 23:49:24 +0100

    refactor: sustituye viejo menu por nuevo

--- assets/js/script.js
+++ assets/js/script.js
@@ -107,3 +107,18 @@ jQuery(document).ready(function ($) {
 
     });
 });
+
+
+
+// selector
+var menu = document.querySelector('.hamburger');
+
+// method
+function toggleMenu(event) {
+    this.classList.toggle('is-active');
+    document.querySelector(".menuppal").classList.toggle("is_active");
+    event.preventDefault();
+}
+
+// event
+menu.addEventListener('click', toggleMenu, false);
